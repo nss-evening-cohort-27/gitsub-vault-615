b9bafbd2a1158f3eb6976b598675568e88ee8ceb
Libannur <99373712+Libannur@users.noreply.github.com>
2024-05-13 19:28:40 -0500

    uncommented pinRepoForm

--- main.js
+++ main.js
@@ -147,22 +147,22 @@ const unpinRepository = (e) => {
 }
 
 //event listener on the form that adds a checked repository to the
-// pinARepositoryForm.addEventListener("change", (e) => {
+pinARepositoryForm.addEventListener("change", (e) => {
 
-//   if (e.target.checked) {
-//     pinRepository(e)
-//     renderCards(pinnedRepositoriesArray, pinnedRepositories)
-//   } else {
-//     unpinRepository(e)
-//     console.log(pinnedRepositoriesArray)
-//     renderCards(pinnedRepositoriesArray, pinnedRepositories)
-//   }
-// })
+  if (e.target.checked) {
+    pinRepository(e)
+    renderCards(pinnedRepositoriesArray, pinnedRepositories)
+  } else {
+    unpinRepository(e)
+    console.log(pinnedRepositoriesArray)
+    renderCards(pinnedRepositoriesArray, pinnedRepositories)
+  }
+})
 
 
 
 // array packages
-
+// 
 const packagesyArray = [
   {
     id: 1,
